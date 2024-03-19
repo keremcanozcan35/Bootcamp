ea022264d975bc0ead6d6737fd6411110803a8bd
kco <keremcanozcan35@gmail.com>
2024-03-19 17:14:15 +0300

    fixed

--- JavaScript/personel.js
+++ JavaScript/personel.js
@@ -2,14 +2,12 @@ let personels = [];
 
 function save(event){
     event.preventDefault();
-    console.log(event);
+
     const firstNameInputElement = document.getElementById("firstName");
     const lasttNameInputElement = document.getElementById("lastName");
     const professionInputElement = document.getElementById("profession");
     const startDateInputElement = document.getElementById("startDate");
     const salaryInputElement = document.getElementById("salary");
-
-
 
     const data = {
         firstName: firstNameInputElement.value,
@@ -21,8 +19,8 @@ function save(event){
 
     personels.push(data);
     console.log(personels);
-    
 
+    setPersonelsToTable();
 }
 
 
@@ -30,24 +28,45 @@ function setPersonelsToTable(){
 
     const tbodyElement = document.querySelector("tbody");
 
-    let value = "";
+    personels = personels.sort((a,b) => a.firstName.localeCompare(b.firstName));
+
+    let value= '';
 
     for(const index in personels){
+        const date = new Date(personels[index].startDate);
+        const newDate = `
+                            ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}
+                        `
+
+        const salary = formatSalary(personels[index].salary.replaceAll(",","."));
         value += `
-        <tr>
-            <td>${+ index + 1 }</td>
-            <td>${personels[index].firstName}</td>
-            <td>${personels[index].lastName}</td>
-            <td>${personels[index].profession}</td>
-            <td>${personels[index].startDate}</td>
-            <td>${personels[index].salary}</td>
-            
-        </tr>
-        
-        
-        `
+                    <tr>
+                        <td>${+ index + 1 }</td>
+                        <td>${personels[index].firstName}</td>
+                        <td>${personels[index].lastName}</td>
+                        <td>${personels[index].profession}</td>
+                        <td>${newDate}</td>
+                        <td>${salary}</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-primary">Update</button>
+                            <button class="btn btn-sm btn-outline-danger">Delete</button>
+                        </td>
+                    </tr>
+                `
     }
 
+    tbodyElement.innerHTML = value;
+}
 
-    const value2 = ``
+
+function formatSalary(salaryString){
+    const salaryNumber = +salaryString;
+
+    const formatter = new Intl.NumberFormat('tr-TR',{
+        style : "currency",
+        currency : "TRY",
+        minimumFractionDigits : 2
+    });
+
+    return formatter.format(salaryNumber);
 }
