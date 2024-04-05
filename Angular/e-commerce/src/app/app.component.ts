c96c2383d1844c8828dd72f62b43c5dced2c1d1c
kco <keremcanozcan35@gmail.com>
2024-04-05 19:21:08 +0300

    ..

--- Angular/e-commerce/src/app/app.component.ts
+++ Angular/e-commerce/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CategoryPipe } from './components/pipes/category.pipe';
+import { CommonModule } from '@angular/common';
+import { ProductPipe } from './components/pipes/product.pipe';
 
 @Component({
   selector: 'app-root',
