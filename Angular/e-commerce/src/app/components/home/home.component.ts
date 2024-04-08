b87d76ccd323fa0dfc77e9deca5acc82e38950db
kco <keremcanozcan35@gmail.com>
2024-04-08 22:59:10 +0300

    08.04.2024

--- Angular/e-commerce/src/app/components/home/home.component.ts
+++ Angular/e-commerce/src/app/components/home/home.component.ts
@@ -8,107 +8,73 @@ import { CategoryModel } from '../models/category.model';
 import { SearchComponent } from '../search/search.component';
 import { TrCurrencyPipe } from 'tr-currency';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule,CategoryPipe, CommonModule, ProductPipe, SearchComponent,TrCurrencyPipe],
+  imports: [FormsModule, CategoryPipe, CommonModule, ProductPipe, SearchComponent, TrCurrencyPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  categories:CategoryModel[] = [
-    {
-      id: "1",
-      name: "Elektronik"
-    },
-    {
-      id: "2",
-      name: "Meyve & Sebze"
-    },
-    {
-      id: "3",
-      name: "Kıyafet"
-    }
-  ];
-
-  products: ProductModel[] = [
-    {
-      id: "1",
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfFFXV4zCJybOFvocqAKKkko37SsPbl9F66Q&usqp=CA",
-      name: "Grundig Pc 2560 B1",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      price: 12650,
-      discountedPrice: 11999,
-      stock: 10,
-      kdvRate: 20,
-      categoryId : "1",
-      category: {
-        id: "1",
-        name: "Elektronik"
-      },
-      quantity: 1
-    },
-    {
-      id: "2",
-      imageUrl: "https://m.media-amazon.com/images/I/61ujQL9JflL._AC_SX679_.jpg",
-      name: "Columbia Delta Ridge™ Down Erkek Mont",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      price: 11550,
-      discountedPrice: 10999,
-      stock: 4,
-      kdvRate: 20,
-      categoryId : "3",
-      category: {
-        id: "3",
-        name: "Kıyafet"
-      },
-      quantity: 1
-    }
-  ]
+  categories: CategoryModel[] = [];
 
   categorySearch: string = "";
   productSearch: string = "";
   selectedCategoryId: string = "";
 
-
-
-  /**
-   *
-   */
-  constructor(private cart: ShoppingCartService) {
-  
+  constructor(private cart: ShoppingCartService,
+    public _product: ProductService
+  ) {
+    setTimeout(() => {
+      this.seedData();
+    }, 1000);
   }
-
-
 
   selectCategory(id: string = "") {
     this.selectedCategoryId = id;
   }
 
-  decrementProductQuantity(product:ProductModel){
-    if(product.quantity > 1){
+  decrementProductQuantity(product: ProductModel) {
+    if (product.quantity > 1) {
       product.quantity--;
     }
   }
 
-  incrementProductQuantity(product:ProductModel){
-    if(product.quantity < product.stock){
+  incrementProductQuantity(product: ProductModel) {
+    if (product.quantity < product.stock) {
       product.quantity++;
     }
   }
 
+  addShoppingCart(product: ProductModel) {
+    const productModel = { ...product };
 
-  addShoppingCart(product:ProductModel){
-    const productModel = {...product};
-
-    const model = this.cart.shoppingCarts.find(x=>x.id == product.id);
-    if(model === undefined){
+    const model = this.cart.shoppingCarts.find(x => x.id == product.id);
+    if (model === undefined) {
       this.cart.shoppingCarts.push(productModel);
-    }else{
+    } else {
       model.quantity += productModel.quantity;
     }
-   
+
     product.stock -= product.quantity;
+  }
+
+  seedData() {
+    this.categories = [
+      {
+        id: "1",
+        name: "Elektronik"
+      },
+      {
+        id: "2",
+        name: "Meyve & Sebze"
+      },
+      {
+        id: "3",
+        name: "Kıyafet"
+      }
+    ];
   }
 }
--- Angular/e-commerce/src/app/components/shopping-carts/shopping-carts.component.ts
+++ Angular/e-commerce/src/app/components/shopping-carts/shopping-carts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { SearchComponent } from '../search/search.component';
 import { HomeComponent } from '../home/home.component';
 import { ProductModel } from '../models/product.model';
@@ -8,47 +8,88 @@ import { ShoppingCartModel } from '../models/shopping-cart.model';
 import { ProductPipe } from '../pipes/product.pipe';
 import { ShoppingCartPipe } from "../pipes/shopping-cart.pipe";
 import { BehaviorSubject } from 'rxjs';
+import { TrCurrencyPipe } from 'tr-currency';
+import { ProductService } from '../../services/product.service';
 
 @Component({
-    selector: 'app-shopping-carts',
-    standalone: true,
-    templateUrl: './shopping-carts.component.html',
-    styleUrl: './shopping-carts.component.css',
-    imports: [SearchComponent, FormsModule, ProductPipe, ShoppingCartPipe]
+  selector: 'app-shopping-carts',
+  standalone: true,
+  templateUrl: './shopping-carts.component.html',
+  styleUrl: './shopping-carts.component.css',
+  imports: [SearchComponent, FormsModule, ProductPipe, ShoppingCartPipe, TrCurrencyPipe]
 })
-@Injectable()
-export class ShoppingCartsComponent {
+export class ShoppingCartsComponent implements OnInit {
 
   shoppingCartSearch: string = '';
   productsFromHomePage: ProductModel[] = [];
+  total: number = 0;
+  totalAmount: number = 0;
+  totalKDV: number = 0;
+  totalKDV1: number = 0;
+  totalKDV10: number = 0;
+  totalKDV20: number = 0;
 
-  constructor(private cart: ShoppingCartService) {
-    this.productsFromHomePage = this.cart.shoppingCarts;
-   
+  constructor(public cart: ShoppingCartService, private _product: ProductService) { }
+
+  ngOnInit(): void {
+    this.calculateTotal();
   }
 
-  increment(product: ProductModel){
-    if(product.quantity < product.stock){
-      product.quantity++;
+  calculateTotal() {
+    this.total = 0;
+    this.totalAmount = 0;
+    this.totalKDV = 0;
+
+    for (const data of this.cart.shoppingCarts) {
+      const amount = data.quantity * data.discountedPrice;
+      const kdv = amount - amount / ((data.kdvRate / 100) + 1);
+      this.totalAmount += amount - kdv;
+      if (data.kdvRate === 1) {
+        this.totalKDV1 += kdv;
+      }
+      else if (data.kdvRate == 10) {
+        this.totalKDV10 += kdv;
+      }
+      else if (data.kdvRate == 20) {
+        this.totalKDV20 += kdv;
+      }
+      this.total += amount;
+
     }
   }
 
-decrement(product: ProductModel){  
-  if(product.quantity > 0){
-    product.quantity--;   
-  }else if(product.quantity === 0){
-    
+  increment(cart: ShoppingCartModel) {
+    const product = this._product.products.find(p => p.id == cart.id);
+    if(product !== undefined) {
+      if (product.stock > 0) {
+        cart.quantity++;
+        product.stock--;
+        this.calculateTotal();
+      }
+    }
   }
 
+  decrement(cart : ProductModel, index: number) {
+    const product = this._product.products.find(p => p.id == cart.id);
+    if(product !== undefined && cart.quantity > 0) {
 
-}
+      if (product.stock > 0) {
+        cart.quantity--;
+        product.stock++;
+        this.calculateTotal();
+      }if(cart.quantity === 0){
+        this.deleteByIndex(index);
+      }
+    }
+  }
 
-deleteByIndex(index : number) {
-  //delete my products from list  
-  this.productsFromHomePage.splice(index,1);
-  
-}
-
-
-
+  deleteByIndex(index: number) {
+    const cart = this.cart.shoppingCarts[index];
+    const data = this._product.products.find(p => p.id == cart.id);
+    if (data !== undefined) {
+      data.stock += cart.quantity;
+    }
+    this.cart.shoppingCarts.splice(index, 1);
+    this.calculateTotal();
+  }
 }
